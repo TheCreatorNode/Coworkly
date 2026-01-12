5017527cfe56ffb90ae46255c85b968e6fbaf0b9
TheCreatorNode <basseyu01@gmail.com>
2026-01-12 14:46:31 +0100

    Add: User model

---
+++ src/api/User/user.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "please provide a valid email"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: [true, "please provide a password"],
+    minlength: 8,
+  },
+
+  confirmPassword: {
+    type: String,
+    required: [true, "please confirm your password"],
+    validate: {
+      validator: function (el) {
+        el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
