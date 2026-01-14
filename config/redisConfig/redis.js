26b9a92c06e2454593552bf22336a64ac6581658
TheCreatorNode <basseyu01@gmail.com>
2026-01-14 15:24:53 +0100

    ADD: redis config file an redis methods

---
+++ config/redisConfig/redis.js
@@ -0,0 +1,23 @@
+const { connect } = require("mongoose");
+const redis = require("redis");
+
+const redisClient = redis.createClient();
+
+redisClient.on("error", (err) => {
+  console.error("Redis client error", err);
+});
+
+redisClient.on("ready", () => {
+  console.log("Redis Client started");
+});
+
+const connectRedis = async () => {
+  if (!redisClient.isOpen) {
+    await redisClient.connect();
+  }
+};
+
+module.exports = {
+  redisClient,
+  connectRedis,
+};
---
+++ config/redisConfig/redis.service.js
@@ -0,0 +1,42 @@
+const { redisClient } = require("./redis");
+
+exports.setCache = async (key, value, ttlseconds = null) => {
+  try {
+    const data = typeof value === "string" ? value : JSON.stringify(value);
+
+    if (ttlseconds) {
+      await redisClient.set(key, value, { EX: ttlseconds });
+    } else {
+      await redisClient.set(key, value);
+    }
+  } catch (error) {
+    return error.message;
+  }
+};
+
+exports.getCache = async (key) => {
+  try {
+    const data = await redisClient.get(key);
+    if (!data) return null;
+
+    return JSON.parse(data);
+  } catch (error) {
+    return error.message;
+  }
+};
+
+exports.deleteCache = async (key) => {
+  try {
+    await redisClient.del(key);
+  } catch (error) {
+    return error.message;
+  }
+};
+
+exports.hasKey = async (key) => {
+  return (await redisClient.exists(key)) === 1;
+};
+
+export const expire = async (key, seconds) => {
+  await redisClient.expire(key, seconds);
+};
